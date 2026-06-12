e7c75afbdc50d75908b28461b670783855b1ede5
DFHACER <longnguyen16051993@gmail.com>
2026-06-12 16:20:44 +0700

    chores: module media

---
+++ backend/common/config/multer.config.ts
@@ -0,0 +1,33 @@
+import { BadRequestException } from "@nestjs/common";
+import { diskStorage } from "multer";
+import { extname } from "path";
+
+
+export const multerOptions = {
+    storage: diskStorage({
+        // Chỉ định nơi lưu file (thư mục gốc /uploads)
+        destination: './uploads',
+        filename: (req, file, cb) => {
+            // Tạo chuỗi ngẫu nhiên từ thời gian hiện tại
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+            // Lấy đuôi file gốc (vd: .jpg, .png)
+            const ext = extname(file.originalname);
+            // Ghép lại thành tên mới (vd: image-1689...123.jpg)
+            cb(null, `image-${uniqueSuffix}${ext}`);
+        },
+    }),
+
+    // Chỉ cho upload file ảnh
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|gif|webp|pdf)$/)) {
+            cb(null, true);
+        } else {
+            cb(new BadRequestException('Chỉ gửi file có định dạng sau (jpg/jpeg/png/gif/webp/pdf)'), false);
+        }
+    },
+
+    // Giới hạn dung lượng 5MB
+    limits: {
+        fileSize: 50 * 1024 * 1024,
+    },
+};
--- backend/src/modules/media/media.controller.ts
+++ backend/src/modules/media/media.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post, Query, Request, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { MediaService } from './media.service';
-import { CreateMediaDto } from './dto/create-media.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerOptions } from 'common/config/multer.config';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PaginationDto } from 'common/dto/pagination.dto';
 import { UpdateMediaDto } from './dto/update-media.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('media')
 export class MediaController {
-  constructor(private readonly mediaService: MediaService) {}
+  constructor(private readonly mediaService: MediaService) { }
 
-  @Post()
-  create(@Body() createMediaDto: CreateMediaDto) {
-    return this.mediaService.create(createMediaDto);
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', multerOptions))
+  async uploadFile(@UploadedFile() file: Express.Multer.File, @Request() req) {
+    if (!file) {
+      throw new BadRequestException('Vui lòng chọn một file hợp lệ để tải lên');
+    }
+
+    return this.mediaService.uploadSingleFile(file, req.user.id);
   }
 
   @Get()
-  findAll() {
-    return this.mediaService.findAll();
+  findAll(@Request() req, @Query() paginationDto: PaginationDto) {
+    return this.mediaService.findAll(req.user, paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.mediaService.findOne(+id);
+  findOne(@Request() req, @Param('id') id: string) {
+    return this.mediaService.findOne(id, req.user);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMediaDto: UpdateMediaDto) {
-    return this.mediaService.update(+id, updateMediaDto);
+  update(@Request() req, @Param('id') id: string, @Body() updateMediaDto: UpdateMediaDto) {
+    return this.mediaService.update(id, updateMediaDto, req.user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mediaService.remove(+id);
+  remove(@Request() req, @Param('id') id: string) {
+    return this.mediaService.remove(id, req.user);
   }
 }
