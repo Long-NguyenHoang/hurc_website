ac6f2fec825660fd19746b31600fdfb0409f7013
DFHACER <longnguyen16051993@gmail.com>
2026-06-16 13:46:01 +0700

    chores: modules contacts

--- backend/src/modules/contacts/contacts.controller.ts
+++ backend/src/modules/contacts/contacts.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from 'common/decorators/roles.decorator';
+import { UserRole } from 'common/enums';
 
 @Controller('contacts')
 export class ContactsController {
-  constructor(private readonly contactsService: ContactsService) {}
+  constructor(private readonly contactsService: ContactsService) { }
 
   @Post()
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactsService.create(createContactDto);
   }
 
-  @Get()
-  findAll() {
-    return this.contactsService.findAll();
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
+  @Get('admin/all')
+  findAllAdmin() {
+    return this.contactsService.findAllAdmin();
   }
 
-  @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
+  @Get('admin/:id')
   findOne(@Param('id') id: string) {
-    return this.contactsService.findOne(+id);
+    return this.contactsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
-    return this.contactsService.update(+id, updateContactDto);
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
+  @Patch('update/:id')
+  update(@Request() req, @Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
+    return this.contactsService.update(id, updateContactDto, req.user.id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.contactsService.remove(+id);
+    return this.contactsService.remove(id);
   }
 }
