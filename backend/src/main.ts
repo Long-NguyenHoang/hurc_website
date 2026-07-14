834b7315789f08db71c879df67ec41cd66a16a32
DFHACER <longnguyen16051993@gmail.com>
2026-07-14 14:28:20 +0700

    CORS CSRF

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,7 +34,9 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://127.0.0.1:3001',
       'http://192.168.1.217',
-      'http://192.168.1.217:3001'
+      'http://192.168.1.217:3001',
+      'https://hurc-website.vercel.app',
+      'https://hurc-website.vercel.app:3001',
     ],
 
     // BẮT BUỘC PHẢI LÀ TRUE: Cho phép Frontend gửi HttpOnly Cookie lên Backend
