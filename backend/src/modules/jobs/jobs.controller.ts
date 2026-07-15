88f80f6bb14cbc24b424d639864dda95ac4c716d
DFHACER <longnguyen16051993@gmail.com>
2026-07-15 16:40:31 +0700

    feat: jobs feature

--- backend/src/modules/jobs/jobs.controller.ts
+++ backend/src/modules/jobs/jobs.controller.ts
@@ -1,34 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Query, UseGuards, Request } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { PaginationDto } from 'common/dto/pagination.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from 'common/decorators/roles.decorator';
+import { UserRole } from 'common/enums';
 
 @Controller('jobs')
 export class JobsController {
-  constructor(private readonly jobsService: JobsService) {}
+  constructor(private readonly jobsService: JobsService) { }
 
-  @Post()
-  create(@Body() createJobDto: CreateJobDto) {
-    return this.jobsService.create(createJobDto);
-  }
-
+  @UseInterceptors(CacheInterceptor)
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  findAllPublic(@Query() paginationDto: PaginationDto) {
+    return this.jobsService.findAllPublic(paginationDto);
   }
 
-  @Get(':id')
+  @UseInterceptors(CacheInterceptor)
+  @Get('details/:slug')
+  findBySlug(@Param('slug') slug: string) {
+    return this.jobsService.findBySlug(slug);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
+  @Post()
+  create(@Request() req, @Body() createJobDto: CreateJobDto) {
+    return this.jobsService.create(createJobDto, req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
+  @Get('admin/all')
+  findAllAdmin(@Query() paginationDto: PaginationDto) {
+    return this.jobsService.findAllAdmin(paginationDto);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
+  @Get('admin/:id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
-    return this.jobsService.update(+id, updateJobDto);
+    return this.jobsService.update(id, updateJobDto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.EDITOR)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.jobsService.remove(+id);
+    return this.jobsService.remove(id);
   }
 }
